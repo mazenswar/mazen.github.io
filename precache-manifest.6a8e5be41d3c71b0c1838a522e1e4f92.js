7eca5a133e11772f8497e94dc193224a46d75f41
mazenswar <mazenswar@protonmail.com>
2019-05-04 11:56:13 -0400

    Updates

---
+++ precache-manifest.6a8e5be41d3c71b0c1838a522e1e4f92.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c563a76184d941b91e4c6a0572f4a5",
+    "url": "/mazen.portfolio/index.html"
+  },
+  {
+    "revision": "38739213797eb2929a5b",
+    "url": "/mazen.portfolio/static/css/main.c4fd5ef2.chunk.css"
+  },
+  {
+    "revision": "71dd08e7c6cc486cddcd",
+    "url": "/mazen.portfolio/static/js/2.a498486d.chunk.js"
+  },
+  {
+    "revision": "38739213797eb2929a5b",
+    "url": "/mazen.portfolio/static/js/main.32d0310f.chunk.js"
+  },
+  {
+    "revision": "16a89ebc4e875e32b6ad",
+    "url": "/mazen.portfolio/static/js/runtime~main.66ee0805.js"
+  },
+  {
+    "revision": "e8a5f8c66877260211d9e76ff9f2925f",
+    "url": "/mazen.portfolio/static/media/GitHub-light.e8a5f8c6.svg"
+  },
+  {
+    "revision": "74c7727fba481eaeebb28bb1241428da",
+    "url": "/mazen.portfolio/static/media/Logo2.74c7727f.png"
+  },
+  {
+    "revision": "dee9bf8cb416ea8c28ca3a9afb0d89b3",
+    "url": "/mazen.portfolio/static/media/al.dee9bf8c.jpg"
+  },
+  {
+    "revision": "f7d78738ceba80ecdeba4fc3659660f1",
+    "url": "/mazen.portfolio/static/media/bahrain.f7d78738.svg"
+  },
+  {
+    "revision": "b7ff67a1992f7b48a4855acdc3122b80",
+    "url": "/mazen.portfolio/static/media/bgl.b7ff67a1.jpg"
+  },
+  {
+    "revision": "28d5c8294f0243b8451ed23fc164180b",
+    "url": "/mazen.portfolio/static/media/dip.28d5c829.png"
+  },
+  {
+    "revision": "57a01dcad8e6288f77d54f8a310acce1",
+    "url": "/mazen.portfolio/static/media/london.57a01dca.svg"
+  },
+  {
+    "revision": "d302ca158b2edd62c80be8d45fcfb27f",
+    "url": "/mazen.portfolio/static/media/newYork.d302ca15.svg"
+  }
+]);
